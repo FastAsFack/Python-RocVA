6dc0969a434fba012c526f71732b29235610b97b
Tijn Bakker <jemoderiseenplopkoek3451@gmail.com>
2024-01-08 10:15:03 +0100

    Made changes to Assignment03 instead of static numbers its now inputs.
    made new directory for week09
    created Exercise01.py
    
    Made all exercises and assignments from 1 to 9 and 1 to 2
    Completed the exercises from 1 to 6

--- Week09/Assignment03.py
+++ Week09/Assignment03.py
@@ -1,2 +1,16 @@
 
+try:
+    num1 = int(input("Enter the 1st positive number: "))
+    num2 = int(input("Enter the 2nd positive number: "))
+
+    if num1 < 0 or num2 < 0:
+        raise ValueError("Please enter positive numbers only.")
+
+    result = num1 * num2
+    print(f"{num1} x {num2} = {result}")
+
+except ValueError as ve:
+    print(f"ValueError: {ve}")
+
+
 
